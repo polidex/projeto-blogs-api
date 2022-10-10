eebc0a9ca80c0ac0ff04aa57b2d15287e4da2191
polidex <lp.igor@gmail.com>
2022-10-10 20:36:47 -0300

    feat: reqt 11 done

---
+++ src/models/PostCategory.js
@@ -0,0 +1,24 @@
+const PostCategory = (sequilize, DataTypes) => {
+  const PostCategory = sequilize.define('PostCategory', {
+    post_id: DataTypes.INTEGER,
+    category_id: DataTypes.INTEGER,
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      foreignKey: 'category_id',
+      otherKey: 'post_id',
+    });
+
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      foreignKey: 'post_id',
+      otherKey: 'category_id',
+    });
+  }
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
